051801a16c39f464e9f3b3b185b55f015fddd68f
Vincent Laferriere <github@monordibogue.com>
2021-02-14 23:00:20 -0500

    now plays the url you give it

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 import DiscordJS from "discord.js";
 import fetch from "node-fetch";
+import { URL } from "url";
 import dotenv from "dotenv";
 import { Readable } from "stream";
 dotenv.config();
 
-(async () => {
-    const {RADIO, TOKEN} = process.env;
-    if (!RADIO || !TOKEN) {
-      console.log("Missing RADIO and/or TOKEN env var");
-      process.exit(1);
+const isURL = (url: string) => {
+    try {
+        new URL(url);
+        return true;
+    } catch (e) {
+        return false;
     }
+};
 
-    const stream = await fetch(RADIO).then((r) => {
-        console.log(r.headers);
-        return r.body;
-    });
+(async () => {
+    const { TOKEN } = process.env;
+    if (!TOKEN) {
+        console.log("Missing TOKEN");
+        process.exit(1);
+    }
 
     const client: DiscordJS.Client = new DiscordJS.Client()
         .on("ready", () => console.log("connected :)"))
@@ -22,30 +27,35 @@ dotenv.config();
             if (message.author.bot || !message.guild || !message.member) return;
             if (!message.content.startsWith("radio ")) return;
             const args = message.content.split(" ").slice(1);
-            switch (args[0]) {
-                case "on":
-                    if (message.guild.me?.voice.connection)
-                        return await message.reply("I am already in a channel");
-                    if (!message.member.voice.channel)
-                        return await message.reply("Join a channel first");
-                    const connection = await message.member.voice.channel.join();
-                    if (!connection)
-                        return await message.reply(
-                            "Unable to join the channel :("
-                        );
+            if (["stop", "leave", "off"].indexOf(args[0]) !== -1) {
+                if (!message.guild.me?.voice.connection)
+                    return await message.reply(
+                        "I am not currently in a channel"
+                    );
+                message.guild.me?.voice.connection.disconnect();
+            } else {
+                if (!isURL(args[0]))
+                    return await message.reply("Give me a full URL to play >:(");
 
-                    connection.play(new Readable().wrap(stream));
-                    break;
-                case "off":
-                    if (!message.guild.me?.voice.connection)
-                        return await message.reply(
-                            "I am not currently in a channel"
-                        );
-                    message.guild.me?.voice.connection.disconnect();
-                    break;
-                default:
-                    await message.reply("radio [on|off]");
-                    break;
+                const resp = await fetch(args[0]);
+                if (
+                    !(resp.headers.get("content-type") || "").startsWith(
+                        "audio/"
+                    )
+                )
+                    return await message.reply(
+                        "Uhh I don't think this is audio.."
+                    );
+
+                if (message.guild.me?.voice.connection)
+                    return await message.reply("I am already in a channel");
+                if (!message.member.voice.channel)
+                    return await message.reply("Join a channel first");
+
+                const connection = await message.member.voice.channel.join();
+                if (!connection)
+                    return await message.reply("Unable to join the channel :(");
+                connection.play(new Readable().wrap(resp.body));
             }
         });
 
